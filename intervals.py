234f21ad8572e310e81a06b91f133a723caa6ded
jisraeli <jisraeli@stanford.edu>
2016-08-30 16:21:33 -0700

    added get_tf_predictive_setup

--- intervals.py
+++ intervals.py
@@ -73,3 +73,73 @@ def bin_bed(bedtool, bin_size, stride):
     windows = bedtool.window_maker(bedtool, w=bin_size, s=stride, i="winnum")
     # pad/remove last window of each interval
     return windows.each(pad_interval, bin_size)
+
+
+def get_tf_predictive_setup(region_bedtool, true_feature_bedtools,
+                            bin_size=200, flank_size=400, stride=50,
+                            filter_flank_overlaps=True,
+                            ambiguous_feature_bedtools=None):
+    """
+    Implements the tf (and general) imputation data setup for a single sample.
+    Assumes genome is hg19.
+    TODO: parallelize intersection calls -takes most of the runtime for multiple feature beds.
+
+    Parameters
+    ----------
+    region_bedtools : list of BedTools
+    tf_feature_peak_bedtools : list of BedTool or None elements
+    ambiguous_feature_bedtools : list of BedTool or None elements, optional
+    """
+    # sanity checks
+    if ambiguous_feature_bedtools is not None:
+        assert len(ambiguous_feature_bedtools) == len(true_feature_bedtools)
+    # bin region_bedtools
+    bins = bin_bed(region_bedtool, bin_size=bin_size, stride=stride)
+    # filter bins to chr1-22,X,Y
+    chrom_list = ["chr%i" % (i) for i in range(1, 23)]
+    chrom_list += ["chrX", "chrY"]
+    bins = BedTool(bins).each(filter_interval_by_chrom, chrom_list)
+    bins = bins.saveas() # save to temp file to enable counting
+    num_bins = bins.count()
+    # set genome to hg19
+    bins = bins.set_chromsizes('hg19')
+    # intersect bins and tf_true_peaks for true labels
+    true_labels_list = []
+    for true_feature_bedtool in true_feature_bedtools:
+        if true_feature_bedtool is not None:
+            true_labels = bed_intersection_labels(bins, true_feature_bedtool)
+        else:
+            true_labels = -1*np.ones((num_bins, 1))
+        true_labels_list.append(true_labels)
+    true_labels = np.concatenate(true_labels_list, axis=1)
+    bins_and_flanks = bins.slop(b=flank_size)
+    if filter_flank_overlaps:
+        # intersect bins and flanks for any overlap  with true features
+        flank_labels_list = []
+        for true_feature_bedtool in true_feature_bedtools:
+            if true_feature_bedtool is not None:
+                flank_labels = bed_intersection_labels(bins, true_feature_bedtool,
+                                                       f=10**-9, F=10**-9)
+            else:
+                flank_labels = np.zeros((num_bins, 1))
+            flank_labels_list.append(flank_labels)
+        flank_labels = np.concatenate(flank_labels_list, axis=1)
+        ## we label negative bins whose flanks have
+        ## any overlap with true features as ambiguous
+        neg_bin_indxs = true_labels == 0
+        neg_bin_pos_flank_indxs = (neg_bin_indxs*(flank_labels == 1)).astype(bool)
+        true_labels[neg_bin_pos_flank_indxs] = -1
+    if ambiguous_feature_bedtools is not None:
+        # intersect bins and ambiguous tfs for ambiguous labels
+        ambg_bin_labels_list = []
+        for amb_feature_bedtools in ambiguous_feature_bedtools:
+            if amb_feature_bedtools is not None:
+                amb_bin_labels = bed_intersection_labels(bins, amb_feature_bedtools)
+            else:
+                amb_bin_labels = np.zeros((num_bins, 1))
+            ambg_bin_labels_list.append(ambg_bin_labels_list)
+        ambg_bin_labels = np.concatenate(ambg_bin_labels_list, axis=1)
+        # we label negative bins that overlap ambiguous feature as ambiguous
+        true_labels[true_labels == 0][ambg_bin_labels == 1] = -1
+        # TODO: do we want to also filter based on any flank overlap with ambiguous features??
+    return bins_and_flanks, true_labels

56c025c5f74cf48e4c0dc5bfd757a06b05148203
jisraeli <jisraeli@stanford.edu>
2016-09-12 12:34:25 -0700

    fixed model loading and saving, added deeplift and predict methods, renamed weighted_cross_entropy to weighted_cross_entropy for compatibility with keras serialization

--- models.py
+++ models.py
@@ -12,7 +12,6 @@ from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.layers.recurrent import GRU
 from keras.regularizers import l1
 
-from dragonn.models import SequenceDNN
 from metrics import ClassificationResult
 
 
@@ -35,12 +34,12 @@ def get_weighted_binary_crossentropy(w0_weights, w1_weights):
     # In addition, weight everything with label -1 to 0
     w0_weights = np.array(w0_weights)
     w1_weights = np.array(w1_weights)
-    def weighted_binary_crossentropy(y_true, y_pred): 
+    def binary_crossentropy(y_true, y_pred): 
         weightsPerTaskRep = y_true * w1_weights[None, :] + (1 - y_true) * w0_weights[None, :]
         nonAmbig = (y_true > -0.5)
         nonAmbigTimesWeightsPerTask = nonAmbig * weightsPerTaskRep
         return K.mean(K.binary_crossentropy(y_pred, y_true) * nonAmbigTimesWeightsPerTask, axis=-1)
-    return weighted_binary_crossentropy
+    return binary_crossentropy
 
 
 class PrintMetrics(Callback):
@@ -53,39 +52,59 @@ class PrintMetrics(Callback):
             print('\n{}\n'.format(self.sequence_DNN.test(self.X_valid, self.y_valid)))
 
 
-class SequenceClassifier(SequenceDNN):
-    def __init__(self, X_train, y_train, use_RNN=False,
+class SequenceClassifier(object):
+    def __init__(self, X=None, y=None, arch_fname=None, weights_fname=None,
                  num_filters=(15, 15, 15), conv_width=(15, 15, 15),
-                 pool_width=35, GRU_size=35, TDD_size=15,
-                 L1=0, dropout=0.0, num_epochs=100, verbose=1):
+                 pool_width=35, L1=0, dropout=0.0,
+                 use_RNN=False, GRU_size=35, TDD_size=15,
+                 num_epochs=100, verbose=1):
         self.saved_params = locals()
-        self.seq_length = X_train.shape[-1]
-        self.input_shape = X_train.shape[1:]
-        self.num_tasks = y_train.shape[1]
-        self.num_epochs = num_epochs
         self.verbose = verbose
-        self.model = Sequential()
-        assert len(num_filters) == len(conv_width)
-        for i, (nb_filter, nb_col) in enumerate(zip(num_filters, conv_width)):
-            conv_height = 4 if i == 0 else 1
-            self.model.add(Convolution2D(
-                nb_filter=nb_filter, nb_row=conv_height,
-                nb_col=nb_col, activation='linear',
-                init='he_normal', input_shape=self.input_shape,
-                W_regularizer=l1(L1), b_regularizer=l1(L1)))
-            self.model.add(Activation('relu'))
-            self.model.add(Dropout(dropout))
-        self.model.add(MaxPooling2D(pool_size=(1, pool_width)))
-        self.model.add(Flatten())
-        self.model.add(Dense(output_dim=self.num_tasks))
-        self.model.add(Activation('sigmoid'))
-        # get task specific class weights
-        task_weights = np.array([class_weights(y_train[:, i]) for i in range(self.num_tasks)])
-        # get weighted cross entropy loss and compile
-        loss_func = get_weighted_binary_crossentropy(task_weights[:, 0], task_weights[:, 1])
-        self.model.compile(optimizer='adam', loss=loss_func)
+        self.num_epochs = num_epochs
+        if arch_fname is not None and weights_fname is not None:
+            from keras.models import model_from_json
+            self.model = model_from_json(open(arch_fname).read())
+            self.model.load_weights(weights_fname)
+            self.num_tasks = self.model.layers[-1].output_shape[-1]
+        elif X is not None and y is not None:
+            self.seq_length = X.shape[-1]
+            self.input_shape = X.shape[1:]
+            self.num_tasks = y.shape[1]
+            self.model = Sequential()
+            assert len(num_filters) == len(conv_width)
+            for i, (nb_filter, nb_col) in enumerate(zip(num_filters, conv_width)):
+                conv_height = X.shape[-2] if i == 0 else 1
+                self.model.add(Convolution2D(
+                    nb_filter=nb_filter, nb_row=conv_height,
+                    nb_col=nb_col, activation='linear',
+                    init='he_normal', input_shape=self.input_shape,
+                    W_regularizer=l1(L1), b_regularizer=l1(L1)))
+                self.model.add(Activation('relu'))
+                self.model.add(Dropout(dropout))
+            self.model.add(MaxPooling2D(pool_size=(1, pool_width)))
+            if use_RNN:
+                num_max_pool_outputs = self.model.layers[-1].output_shape[-1]
+                self.model.add(Reshape((num_filters[-1], num_max_pool_outputs)))
+                self.model.add(Permute((2, 1)))
+                self.model.add(GRU(GRU_size, return_sequences=True))
+                self.model.add(TimeDistributedDense(TDD_size, activation='linear'))
+            self.model.add(Flatten())
+            self.model.add(Dense(output_dim=self.num_tasks))
+            self.model.add(Activation('sigmoid'))
+            # get task specific class weights
+            task_weights = np.array([class_weights(y[:, i]) for i in range(self.num_tasks)])
+            # get weighted cross entropy loss and compile
+            loss_func = get_weighted_binary_crossentropy(task_weights[:, 0], task_weights[:, 1])
+            self.model.compile(optimizer='adam', loss=loss_func)
+        else:
+            raise RuntimeError("SequenceDNN initialization requires X/y data or arch/weights files!")
 
-    def train(self, X, y, validation_data, patience=5, verbose=True, batch_size=128):
+    def train(self, X, y, validation_data,
+              patience=5, verbose=True, batch_size=128, reweigh_loss_func=False):
+        if reweigh_loss_func:
+            task_weights = np.array([class_weights(y[:, i]) for i in range(y.shape[1])])
+            loss_func = get_weighted_binary_crossentropy(task_weights[:, 0], task_weights[:, 1])
+            self.model.compile(optimizer='adam', loss=loss_func)
         # define callbacks and fit
         self.callbacks = [EarlyStopping(monitor='val_loss', patience=patience)]
         if verbose:
@@ -95,10 +114,40 @@ class SequenceClassifier(SequenceDNN):
             validation_data=validation_data,
             callbacks=self.callbacks, verbose=verbose)
 
+    def deeplift(self, X, batch_size=200):
+        """
+        Returns (num_task, num_samples, 1, num_bases, sequence_length) deeplift score array.
+        """
+        assert len(np.shape(X)) == 4 and np.shape(X)[1] == 1
+        from deeplift import keras_conversion as kc
+        from deeplift.blobs import MxtsMode
+        # normalize sequence convolution weights
+        kc.mean_normalise_first_conv_layer_weights(self.model, None)
+        # run deeplift
+        deeplift_model = kc.convert_sequential_model(
+            self.model, mxts_mode=MxtsMode.DeepLIFT)
+        target_contribs_func = deeplift_model.get_target_contribs_func(
+            find_scores_layer_idx=0)
+        return np.asarray([
+            target_contribs_func(task_idx=i, input_data_list=[X],
+                                 batch_size=batch_size, progress_update=None)
+            for i in range(self.num_tasks)])
+
+    def save(self, prefix):
+        arch_fname = prefix + '.arch.json'
+        weights_fname = prefix + '.weights.h5'
+        open(arch_fname, 'w').write(self.model.to_json())
+        self.model.save_weights(weights_fname, overwrite=True)
+
     def test(self, X, y):
         return ClassificationResult(y, self.predict(X))
 
     def score(self, X, y, metric):
         return self.test(X, y)[metric]
+
+    def predict(self, X):
+        return self.model.predict(X, batch_size=128, verbose=False)
+
+    
         
     

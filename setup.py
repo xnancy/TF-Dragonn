fd161d541feaa6a5caf843882158b21ad1a1d2b2
jisraeli <jisraeli@stanford.edu>
2016-09-20 17:45:47 -0700

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+config = {
+    'include_package_data': True,
+    'description': 'TF modeling with Deep RegulAtory GenOmic Neural Networks (TF-DragoNN)',
+    'download_url': 'https://github.com/kundajelab/tf-dragonn',
+    'version': '0.1.0',
+    'packages': ['tfdragonn'],
+    'setup_requires': [],
+    'install_requires': ['numpy>=1.9', 'keras==0.3.2', 'deeplift', 'sklearn'],
+    'dependency_links': ['https://github.com/kundajelab/deeplift/tarball/master#egg=deeplift-0.2'],
+    'scripts': [],
+    'name': 'tfdragonn'
+}
+
+if __name__== '__main__':
+    setup(**config)

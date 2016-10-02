31eeb503e60c65cc2ae2152274f748a7f7b68fb5
jisraeli <jisraeli@stanford.edu>
2016-10-02 16:31:23 -0700

    updated SequenceClassifier and StreamingSequenceClassifier; added tfdragonn test command

--- tfdragonn/__main__.py
+++ tfdragonn/__main__.py
@@ -53,6 +53,12 @@ def parse_args():
                                                       multiprocessing_parser,
                                                       prefix_parser],
                                              help='interpretation help')
+    test_parser = subparsers.add_parser('test',
+                                        parents=[data_config_parser,
+                                                 model_files_parser,
+                                                 multiprocessing_parser,
+                                                 prefix_parser],
+                                        help='model testing help')
     # return command and command arguments
     args = vars(parser.parse_args())
     command = args.pop("command", None)
@@ -102,8 +108,11 @@ def main_train(data_config_file=None,
         fasta_extractor = MemmappedFastaExtractor(data['genome_data_dir'])
         logger.info("Initializing a StreamingSequenceClassifer")
         model = StreamingSequenceClassifier(interval_length, num_tasks, **architecture_parameters)
+        logger.info("Compiling StreamingSequenceClassifer..")
+        model.compile(y=y_train)
         logger.info("Starting to train with streaming data..")
-        model.train(intervals_train, y_train, intervals_valid, y_valid, fasta_extractor)
+        model.train(intervals_train, y_train, intervals_test, y_test, fasta_extractor,
+                    save_best_model_to_prefix=prefix)
     else: # extract encoded data in memory
         logger.info("extracting data in memory")
         fasta_extractor = FastaExtractor(genome_fasta)
@@ -113,8 +122,10 @@ def main_train(data_config_file=None,
         X_train = fasta_extractor(intervals_train)
         # initialize model and train
         ## TODO: load pretrained model and/or architecture
-        logger.info("Initializing a SequenceClassifer")
+        logger.info("Initializing a SequenceClassifer..")
         model = SequenceClassifier(interval_length, num_tasks, **architecture_parameters)
+        logger.info("Compiling SequenceClassifer..")
+        model.compile(y=y_train)
         logger.info("Starting to train")
         model.train(X_train, y_train, (X_test, y_test), patience=6)
     model.save(prefix)
@@ -130,8 +141,51 @@ def main_interpret(data_config_file=None,
     from dlutils import write_deeplift_track
     # get matched region beds and feature/tf beds
     logger.info("parsing data config file..")
-    region_bed, feature_beds, genome_fasta = parse_data_config_file(data_config_file)
+    data = parse_data_config_file(data_config_file)
     # get regions and labels, split into train/test
+    if os.path.isfile("{}.intervals.bed".format(prefix)) and os.path.isfile("{}.labels.npy".format(prefix)):
+        logger.info("loading intervals from {}.intervals.bed".format(prefix))
+        regions = BedTool("{}.intervals.bed".format(prefix))
+        logger.info("loading labels from {}.labels.npy".format(prefix))
+        labels = np.load("{}.labels.npy".format(prefix))
+    else:
+        logger.info("getting regions and labels")
+        regions, labels = get_tf_predictive_setup(data['feature_beds'], region_bedtool=data['region_bed'],
+                                                  bin_size=200, flank_size=400, stride=200,
+                                                  filter_flank_overlaps=False, genome='hg19', n_jobs=n_jobs,
+                                                  save_to_prefix=prefix)
+    if data['genome_data_dir'] is not None: # use a streaming model                                                                                                                  
+        fasta_extractor = MemmappedFastaExtractor(data['genome_data_dir'])
+        logger.info("Initializing a StreamingSequenceClassifer")
+    else:
+        logger.info("loading model...")
+        model = SequenceClassifier(arch_fname=arch_file,
+                                   weights_fname=weights_file)
+        # extract encoded data in memory
+        logger.info("extracting data from regions..")
+        fasta_extractor = FastaExtractor(genome_fasta)
+        X = fasta_extractor(regions)
+        logger.info("running deeplift..")
+        dl_scores = model.deeplift(X, batch_size=1000)
+    logger.info("starting to write deeplift scores")
+    for i, task_dl_scores in enumerate(dl_scores):
+        logger.info("writing deeplift score track for task {}".format(str(i)))
+        write_deeplift_track(task_dl_scores, regions, "{}.task{}_dl_track".format(prefix, str(i)),
+                             merge_type='max',
+                             CHROM_SIZES='/mnt/data/annotations/by_release/hg19.GRCh37/hg19.chrom.sizes')
+    logger.info("Done!")
+
+
+def main_test(data_config_file=None,
+              prefix=None,
+              n_jobs=None,
+              arch_file=None,
+              weights_file=None):
+    from dlutils import write_deeplift_track
+    # get matched region beds and feature/tf beds
+    logger.info("parsing data config file..")
+    data = parse_data_config_file(data_config_file)
+    # get regions and labels
     if os.path.isfile("{}.intervals.bed".format(prefix)) and os.path.isfile("{}.labels.npy".format(prefix)):
         logger.info("loading intervals from {}.intervals.bed".format(prefix))
         regions = BedTool("{}.intervals.bed".format(prefix))
@@ -143,31 +197,40 @@ def main_interpret(data_config_file=None,
                                                   bin_size=200, flank_size=400, stride=200,
                                                   filter_flank_overlaps=False, genome='hg19', n_jobs=n_jobs,
                                                   save_to_prefix=prefix)
-    logger.info("loading model...")
-    model = SequenceClassifier(arch_fname=arch_file,
-                               weights_fname=weights_file)
-    # extract encoded data in memory
-    logger.info("extracting data from regions..")
-    fasta_extractor = FastaExtractor(genome_fasta)
-    X = fasta_extractor(regions)
-    logger.info("running deeplift..")
-    dl_scores = model.deeplift(X, batch_size=1000)
-    logger.info("starting to write deeplift scores")
-    for i, task_dl_scores in enumerate(dl_scores):
-        logger.info("writing deeplift score track for task {}".format(str(i)))
-        write_deeplift_track(task_dl_scores, regions, "{}.task{}_dl_track".format(prefix, str(i)),
-                             merge_type='max',
-                             CHROM_SIZES='/mnt/data/annotations/by_release/hg19.GRCh37/hg19.chrom.sizes')
-    logger.info("Done!")
+    # get test subset
+    ## TODO: user-specified chromosomes
+    _, intervals_test, _, y_test = train_test_chr_split(regions, labels, ["chr1", "chr2"])
+    if data['genome_data_dir'] is not None: # use a streaming model
+        fasta_extractor = MemmappedFastaExtractor(data['genome_data_dir'])
+        logger.info("loading a StreamingSequenceClassifier model...")
+        model = StreamingSequenceClassifier(arch_fname=arch_file,
+                                   weights_fname=weights_file)
+        logger.info("Testing the model...")
+        test_results = model.test(intervals_test, y_test, fasta_extractor)
+    else: # extract encoded data in memory
+        logger.info("loading a SequenceClassifier model...")
+        model = SequenceClassifier(arch_fname=arch_file,
+                                   weights_fname=weights_file)
+        # extract encoded data in memory
+        logger.info("extracting data from regions..")
+        fasta_extractor = FastaExtractor(genome_fasta)
+        logger.info("extracting data in memory")
+        fasta_extractor = FastaExtractor(genome_fasta)
+        logger.info("extracting test data")
+        X_test = fasta_extractor(intervals_test)
+        logger.info("Testing the model...")
+        test_results = model.test(X_test, y_test)
+    logger.info("Test results:\n{}".format(test_results))
 
 
 def main():
     # parse args
     command_functions = {'train': main_train,
-                         'interpret': main_interpret}
+                         'interpret': main_interpret,
+                         'test': main_test}
     command, args = parse_args()
     # perform theano/keras import
-    global SequenceClassifier
-    from .models import SequenceClassifier
+    global SequenceClassifier, StreamingSequenceClassifier
+    from .models import SequenceClassifier, StreamingSequenceClassifier
     # run command
     command_functions[command](**args)
--- tfdragonn/models.py
+++ tfdragonn/models.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function
 import itertools
 import numpy as np
+from builtins import zip
 
 from keras import backend as K
 from keras.models import Sequential
@@ -37,11 +38,14 @@ def infinite_batch_iter(iterable, batch_size):
                       batch_size)
 
 
-def generate_signals_from_intervals(intervals, extractor, batch_size=128):
+def generate_signals_from_intervals(intervals, extractor, batch_size=128, indefinitely=True):
     """
     Generates signals extracted on interval batches.
     """
-    batch_iterator = infinite_batch_iter(intervals, batch_size)
+    if indefinitely:
+        batch_iterator = infinite_batch_iter(intervals, batch_size)
+    else:
+        batch_iterator = batch_iter(intervals, batch_size)
     for batch_intervals in batch_iterator:
         yield extractor(batch_intervals)
 
@@ -110,11 +114,11 @@ class SequenceClassifier(object):
             self.model = Sequential()
             assert len(num_filters) == len(conv_width)
             for i, (nb_filter, nb_col) in enumerate(zip(num_filters, conv_width)):
-                conv_height = X.shape[-2] if i == 0 else 1
+                conv_height = 4 if i == 0 else 1
                 self.model.add(Convolution2D(
                     nb_filter=nb_filter, nb_row=conv_height,
                     nb_col=nb_col, activation='linear',
-                    init='he_normal', input_shape=(1, 4, seq_length),
+                    init='he_normal', input_shape=(1, conv_height, seq_length),
                     W_regularizer=l1(L1), b_regularizer=l1(L1)))
                 self.model.add(Activation('relu'))
                 self.model.add(Dropout(dropout))
@@ -128,13 +132,25 @@ class SequenceClassifier(object):
             self.model.add(Flatten())
             self.model.add(Dense(output_dim=self.num_tasks))
             self.model.add(Activation('sigmoid'))
-            # get task specific class weights
-            task_weights = np.array([class_weights(y[:, i]) for i in range(self.num_tasks)])
-            # get weighted cross entropy loss and compile
-            loss_func = get_weighted_binary_crossentropy(task_weights[:, 0], task_weights[:, 1])
-            self.model.compile(optimizer='adam', loss=loss_func)
         else:
             raise RuntimeError("Model initialization requires seq_length and num_tasks or arch/weights files!")
+
+    def compile(self, optimizer='adam', y=None):
+        """
+        Defines learning parameters and compiles the model.
+
+        Parameters
+        ----------
+        y : 2darray, optional
+           Uses class-weighted cross entropy loss if provides.
+           Otherwise, uses non-weighted cross entropy loss.
+        """
+        if y is not None:
+            task_weights = np.array([class_weights(y[:, i]) for i in range(y.shape[1])])
+            loss_func = get_weighted_binary_crossentropy(task_weights[:, 0], task_weights[:, 1])
+        else:
+            loss_func='binary_crossentropy'
+        self.model.compile(optimizer=optimizer, loss=loss_func)
 
     def train(self, X, y, validation_data,
               patience=5, verbose=True, batch_size=128, reweigh_loss_func=False):
@@ -186,29 +202,92 @@ class SequenceClassifier(object):
     def predict(self, X, batch_size=128):
         return self.model.predict(X, batch_size=batch_size, verbose=False)
 
+
+class PrintIntervalMetrics(Callback):
+
+        def __init__(self, intervals, y, extractor, sequence_DNN):
+            self.intervals = intervals
+            self.y = y
+            self.extractor = extractor
+            self.sequence_DNN = sequence_DNN
+
+        def on_epoch_end(self, epoch, logs={}):
+            print('\n{}\n'.format(self.sequence_DNN.test(
+                self.intervals, self.y, self.extractor)))
+
     
 class StreamingSequenceClassifier(SequenceClassifier):
 
     def train(self, intervals_train, y_train,
               intervals_valid, y_valid, fasta_extractor,
-              patience=5, verbose=True, batch_size=128, reweigh_loss_func=False):
+              save_best_model_to_prefix=None,
+              early_stopping_metric='auPRC', num_epochs=100,
+              batch_size=128, epoch_size=50000,
+              early_stopping_patience=5, verbose=True, reweigh_loss_func=False):
+        ## TODO: run predict loop only once, early stop based on performance metrics
         if reweigh_loss_func:
             task_weights = np.array([class_weights(y[:, i]) for i in range(y.shape[1])])
             loss_func = get_weighted_binary_crossentropy(task_weights[:, 0], task_weights[:, 1])
             self.model.compile(optimizer='adam', loss=loss_func)
-        # define callbacks
-        self.callbacks = [EarlyStopping(monitor='val_loss', patience=patience)]
-        if verbose:
-            self.callbacks.append(PrintMetrics(validation_data, self))
         # define generators
         training_generator = zip(generate_signals_from_intervals(intervals_train, fasta_extractor),
                                  generate_array_batches(y_train, batch_size=batch_size))
-        validation_generator = zip(generate_signals_from_intervals(intervals_valid, fasta_extractor),
-                                   generate_array_batches(labels_valid, batch_size=batch_size))
-        self.model.fit_generator(training_generator, 50000, 4,
+        #validation_generator = zip(generate_signals_from_intervals(intervals_valid, fasta_extractor),
+        #                           generate_array_batches(y_valid, batch_size=batch_size))
+        # define callbacks and fit
+        """
+        callbacks = [EarlyStopping(monitor='val_loss', patience=patience)]
+        if verbose:
+            callbacks.append(PrintIntervalMetrics(intervals_valid, y_valid, fasta_extractor, self))
+        self.model.fit_generator(training_generator,
+                                 samples_per_epoch=50000,
+                                 nb_epoch=100,
                                  validation_data=validation_generator,
-                                 nb_val_samples=len(intervals_valid))
+                                 nb_val_samples=len(y_valid),
+                                 callbacks=callbacks)
+        """
+        valid_metrics = []
+        best_metric = np.inf if early_stopping_metric == 'Loss' else -np.inf
+        samples_per_epoch = len(y_train) if epoch_size is None else epoch_size
+        for epoch in range(1, num_epochs + 1):
+            self.model.fit_generator(training_generator,
+                                     samples_per_epoch=samples_per_epoch,
+                                     nb_epoch=1)
+            epoch_valid_metrics = self.test(intervals_valid, y_valid, fasta_extractor)
+            valid_metrics.append(epoch_valid_metrics)
+            if verbose:
+                print('Epoch {}:'.format(epoch))
+                print('{}'.format(epoch_valid_metrics), end='')
+            current_metric = epoch_valid_metrics[early_stopping_metric].mean()
+            if (early_stopping_metric == 'Loss') == (current_metric <= best_metric):
+                if verbose:
+                    print('New best {}. Saving model.\n'.format(early_stopping_metric))
+                best_metric = current_metric
+                best_epoch = epoch
+                early_stopping_wait = 0
+                if save_best_model_to_prefix is not None:
+                    self.save(save_best_model_to_prefix)
+            else:
+                if early_stopping_wait >= early_stopping_patience:
+                    break
+                early_stopping_wait += 1
+        if verbose: # end of training messages
+            print('Finished training after {} epochs.'.format(epoch))
+            if save_best_model_to_prefix is not None:
+                print("The best model's architecture and weights (from epoch {0}) "
+                      'were saved to {1}.arch.json and {1}.weights.h5'.format(
+                    best_epoch, save_best_model_to_prefix))
 
     def predict(self, intervals, fasta_extractor, batch_size=128):
-        generator = generate_signals_from_intervals(intervals, fasta_extractor)
-        return self.model.predict_generator(self, generator, len(intervals))
+        """
+        Generates data and returns a single 2d array with predictions.
+        """
+        generator = generate_signals_from_intervals(
+            intervals, fasta_extractor, indefinitely=False)
+        predictions = [np.vstack(self.model.predict_on_batch(batch))
+                       for batch in generator]
+        return np.vstack(predictions)
+
+    def test(self, intervals, y, fasta_extractor):
+        predictions = self.predict(intervals, fasta_extractor)
+        return ClassificationResult(y, predictions)

bd507543c0e5824ecad0c6afb766f69530f6e9e2
Chris Probert <chris@DNab4210a2.stanford.edu>
2016-11-27 22:01:24 -0800

    implemented minimal dataset parser

---
+++ tfdragonn/tensorflow/datasets.py
@@ -0,0 +1,44 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import json
+
+import numpy as np
+from pybedtools import BedTool
+
+
+def parse_inputs_and_intervals(processed_inputs_file, processed_intervals_file):
+    all_datasets = {}
+    with open(processed_inputs_file, 'r') as fp:
+        data = json.load(fp)
+    for cur_dataset_id, cur_dataset_dict in data.items():
+        all_datasets[cur_dataset_id] = {}
+        if 'dnase_data_dir' in cur_dataset_dict:
+            all_datasets[cur_dataset_id][
+                'dnase_data_dir'] = cur_dataset_dict['dnase_data_dir']
+        if 'genome_data_dir' in cur_dataset_dict:
+            all_datasets[cur_dataset_id][
+                'genome_data_dir'] = cur_dataset_dict['genome_data_dir']
+
+    dataset = {}
+
+    with open(processed_intervals_file, 'r') as fp:
+        data = json.load(fp)
+    task_names = data['task_names']
+    for dataset_id, dataset_dict in data.items():
+        if dataset_id == 'task_names':
+            continue
+        dataset[dataset_id] = all_datasets[dataset_id]
+
+        # transpose for efficent slicing
+        labels_mtx = np.load(dataset_dict['labels']).T
+        labels_dict = {task_id: labels_mtx[i, :]
+                       for i, task_id in enumerate(task_names)}
+
+        dataset[dataset_id]['labels'] = labels_dict
+
+        bt = BedTool(dataset_dict['regions'])
+        regions_dict = {k: bt.to_dataframe()[k].as_matrix() for k in [
+            'chrom', 'start', 'end']}
+        dataset[dataset_id]['regions'] = regions_dict

595da9bb59693addb2de1a8e38f4862f20554282
Chris Probert <chris@DN51skqs.SUNet>
2016-11-29 20:53:49 -0800

    add notebook to generate test data

--- tfdragonn/tensorflow/interval_queue.py
+++ tfdragonn/tensorflow/interval_queue.py
@@ -16,16 +16,16 @@ def interval_queue(intervals, labels, dequeue_size=128, name='interval-queue'):
 
     Args:
         intervals: a dict of numpy arrays, with the schema:
-            'chrom': (string), 'start': (int), 'stop' (int)
+            'chrom': (string), 'start': (int), 'end' (int)
         labels: a dict of numpy arrays of size N, where N == len(intervals['chrom'])
         name: (optional) string, name for this queue
     Returns:
-        a dictionary of tensors with keys [chrom, start, stop, labels], where each
+        a dictionary of tensors with keys [chrom, start, end, labels], where each
         tensor has shape equal to dequeue_size. labels is a dictionary of tensors, where
         the keys are the same as in the provided labels dict.
     """
     n_exs = next(labels.values()).shape[0]
-    for k in ['chrom', 'start', 'stop']:
+    for k in ['chrom', 'start', 'end']:
         if k not in intervals:
             raise IOError('intervals must contain {}'.format(k))
         elif intervals[k].shape[0] != n_exs:
@@ -37,8 +37,8 @@ def interval_queue(intervals, labels, dequeue_size=128, name='interval-queue'):
             k['chrom'], shuffle=False, capacity=_DEFAULT_BUFFER_CAPACITY, name='chrom-buffer')
         start_queue = tf.train.input_producer(
             k['start'], shuffle=False, capacity=_DEFAULT_BUFFER_CAPACITY, name='start-buffer')
-        stop_queue = tf.train.input_producer(
-            k['stop'], shuffle=False, capacity=_DEFAULT_BUFFER_CAPACITY, name='stop-buffer')
+        end_queue = tf.train.input_producer(
+            k['end'], shuffle=False, capacity=_DEFAULT_BUFFER_CAPACITY, name='end-buffer')
 
         labels_dequeues = {}
         for label_name, label in labels.items():
@@ -49,7 +49,7 @@ def interval_queue(intervals, labels, dequeue_size=128, name='interval-queue'):
         outputs = {
             'chrom': chrom_queue.dequeue_many(dequeue_size),
             'start': start_queue.dequeue_many(dequeue_size),
-            'stop': stop_queue.dequeue_many(dequeue_size),
+            'end': stop_queue.dequeue_many(dequeue_size),
             'labels': labels_dequeues,
         }
 

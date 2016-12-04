931d1d2e722027d188ea576b49ad31a60851a2ae
Chris Probert <chris@DN51sljf.SUNet>
2016-12-04 11:54:20 -0800

    refactor shared queue to class

--- tfdragonn/tensorflow/shared_examples_queue.py
+++ tfdragonn/tensorflow/shared_examples_queue.py
@@ -2,59 +2,68 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from collections import OrderedDict
-from collections import defaultdict
-
 import tensorflow as tf
 
-"""
-A shared examples queue for multiple datasets.
-"""
 
+class SharedExamplesQueue(object):
+    """A container for a shared examples queue and metadata."""
 
-def get_shared_batch(readers, batch_size=128, shuffle=True, capacity=5000,
-                     name='shared-examples-queue'):
-    """Generate a batch of examples from all datasets given a dict of their readers.
-    Args:
-        readers: a dictionary of readers, returned by `get_readers`
-        batch_size: the size of batched tensors to return
-        shuffle: whether to shuffle the returned batches
-        capacity: the size of the buffer used for batching
-        name: the namespace used
-    Returns:
-        batch: a dictionary of tensors with first dimension batch_size
+    def __init__(self, readers, batch_size=128, shuffle=True, capacity=5000,
+                 name='shared-examples-queue'):
+        """Construct a shared examples queue for a dict of readers.
 
-    For consitency, each reader must have the same fields.
-    """
-    reader_keys = list(readers.keys())  # keep order consistent
-    data_keys = set()
-    for reader in readers.values():
-        data_keys = data_keys.union(reader.names)
-    data_keys = list(data_keys)
+        Args:
+            readers: a dictionary of readers, returned by `get_readers`
+            batch_size: the size of batched tensors to return
+            shuffle: whether to shuffle the returned batches
+            capacity: the size of the buffer used for batching
+            name: the namespace used
+        Returns:
+            batch: a dictionary of tensors with first dimension batch_size
 
-    dtypes_and_shapes = OrderedDict
-    for r_name, r in readers.items():
-        for name, dtype, shape in zip(r.names, r.dtypes, r.shapes):
-            dtypes_and_shapes[name] = (dtype, shape)
+        For consitency, each reader must have the same fields.
+        """
+        self.name = name
+        # keep order consistent
+        self.reader_keys = sorted(list(readers.keys()))
+        data_keys = set()
+        for reader in readers.values():
+            data_keys = data_keys.union(reader.names)
+        self.data_keys = list(data_keys)
 
-    with tf.variable_scope(name):
-        dequeues = {k: v.dequeue() for k, v in readers.items()}
-        tensors_to_enqueue = {}
-        for tensor_name in data_keys:
-            values = [dequeues[k][tensor_name] for k in reader_keys]
-            tensors_to_enqueue[tensor_name] = tf.stack(values)
+        with tf.variable_scope(name):
+            dequeues = {k: v.dequeue() for k, v in readers.items()}
+            tensors_to_enqueue = {}
+            for tensor_name in data_keys:
+                values = [dequeues[k][tensor_name] for k in self.reader_keys]
+                tensors_to_enqueue[tensor_name] = tf.stack(values)
 
-        # Add the dataset names so we can evaluate per-dataset
-        tensors_to_enqueue['dataset'] = tf.convert_to_tensor(reader_keys)
+            # Add the dataset names so we can evaluate per-dataset
+            tensors_to_enqueue['dataset'] = tf.convert_to_tensor(
+                self.reader_keys)
 
-        if shuffle:
-            min_after_dequeue = int(0.5 * capacity)
-            outputs = tf.train.shuffle_batch(tensors_to_enqueue, batch_size, capacity=capacity,
-                                             min_after_dequeue=min_after_dequeue, num_threads=4,
-                                             enqueue_many=True)
+            # Also add the dataset indexes to make it easier to split by
+            # dataset
+            tensors_to_enqueue[
+                'dataset-index'] = tf.range(len(self.reader_keys))
 
-        else:
-            outputs = tf.train.batch(tensors_to_enqueue, batch_size, num_threads=4,
-                                     capacity=capacity, enqueue_many=True)
+            if shuffle:
+                min_after_dequeue = int(0.5 * capacity)
+                self._outputs = tf.train.shuffle_batch(
+                    tensors_to_enqueue, batch_size, capacity=capacity,
+                    min_after_dequeue=min_after_dequeue, num_threads=4, enqueue_many=True)
 
-        return outputs
+            else:
+                self._outputs = tf.train.batch(
+                    tensors_to_enqueue, batch_size, num_threads=4, capacity=capacity,
+                    enqueue_many=True)
+
+        @property
+        def outputs(self):
+            """The outputs of the shared examples queue."""
+            return self._outputs
+
+        @property
+        def dataset_labels(self):
+            """The dataset labels that `dataset-index` corresponds to."""
+            return self.reader_keys

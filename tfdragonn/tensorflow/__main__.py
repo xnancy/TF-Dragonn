3f6ef88ea3446e4a969ed2efeaa082a9e815e56e
jisraeli <jisraeli@stanford.edu>
2016-12-14 21:46:52 -0800

    added extraction of distances to gencode features

--- tfdragonn/tensorflow/__main__.py
+++ tfdragonn/tensorflow/__main__.py
@@ -9,7 +9,8 @@ import numpy as np
 import os
 
 from genomedatalayer.shm import (
-    extract_fasta_to_file, extract_bigwig_to_file, extract_bed_counts_to_file
+    extract_fasta_to_file, extract_bigwig_to_file,
+    extract_bed_counts_to_file, extract_dist_to_intervals_to_file
 )
 
 from tfdragonn.datasets import parse_raw_intervals_config_file
@@ -29,7 +30,12 @@ logger.propagate = False
 
 input2extractor_func = {'dnase_bigwig': extract_bigwig_to_file,
                         'genome_fasta': extract_fasta_to_file,
-                        'dnase_peaks_bed': extract_bed_counts_to_file}
+                        'dnase_peaks_bed': extract_bed_counts_to_file,
+                        'gencode_tss': extract_dist_to_intervals_to_file,
+                        'gencode_annotation': extract_dist_to_intervals_to_file,
+                        'gencode_polyA': extract_dist_to_intervals_to_file,
+                        'gencode_lncRNA': extract_dist_to_intervals_to_file,
+                        'gencode_tRNA': extract_dist_to_intervals_to_file}
 
 def parse_args():
     parser = argparse.ArgumentParser(

0f54ea53bd8350227ce678c5fb5eac61e561cdc1
Chris Probert <csprobert@gmail.com>
2016-12-16 12:18:42 -0800

    initial refactor of trainers

--- tfdragonn/tensorflow/dataset_interval_reader.py
+++ tfdragonn/tensorflow/dataset_interval_reader.py
@@ -54,10 +54,9 @@ def get_readers_and_tasks(processed_inputs_file, processed_intervals_file, name=
     return examples_queues, task_names
 
 
-def get_readers_and_tasks_with_holdouts(processed_inputs_file, processed_intervals_file,
-                                        validation_chroms=[], holdout_chroms=[],
-                                        name='data-readers', no_validation_queues=False,
-                                        in_memory=True):
+def get_train_readers_and_tasknames(processed_inputs_file, processed_intervals_file,
+                                    validation_chroms=[], holdout_chroms=[],
+                                    name='data-readers', in_memory=True):
     """Generate two reader and examples queues for train/validation of each dataset.
 
     Args:
@@ -66,7 +65,7 @@ def get_readers_and_tasks_with_holdouts(processed_inputs_file, processed_interva
         validation_chroms: (optional) list of chrom strings to provide as validation examples
         holdout_chroms: (optional) list of heldout chrom strings that will not be provided
 
-    Returns: a 3-tuple of (train_readers, validation_readers, task_names):
+    Returns: a tuple of (train_readers, task_names):
 
         train_readers: a dictionary of an examples queue for each dataset. Each queue contains:
             intervals/start (int) - the start coordinate of the intervals
@@ -77,8 +76,6 @@ def get_readers_and_tasks_with_holdouts(processed_inputs_file, processed_interva
 
             iff labels are specified in the `processed_intervals_file`, also:
             labels - the labels for each interval for each task (size T)
-        validation_readers: a dictionary of an examples queue for each dataset.
-            Same format as train_readers, but only for validation_chroms regions.
         task names:
             a list of strings of task names, of size T
     """
@@ -95,37 +92,88 @@ def get_readers_and_tasks_with_holdouts(processed_inputs_file, processed_interva
         task_names = datasets[list(datasets.keys())[0]]['task_names']
 
         train_examples_queues = {}
-        valid_examples_queues = {}
         for dataset_id, dataset in datasets.items():
             intervals = dataset['intervals']
 
             validation_idxs = np.in1d(intervals['chrom'], validation_chroms)
             holdout_idxs = np.in1d(intervals['chrom'], holdout_chroms)
             train_idxs = ~ np.logical_or(validation_idxs, holdout_idxs)
-
             train_intervals = {k: intervals[k][train_idxs]
-                               for k in ['chrom', 'start', 'end']}
-            valid_intervals = {k: intervals[k][validation_idxs]
                                for k in ['chrom', 'start', 'end']}
 
             datafiles = dataset['inputs']
             labels = None
             train_labels = None
-            valid_labels = None
             if 'labels' in dataset.keys():
                 labels = dataset['labels']
                 train_labels = labels[train_idxs]
-                valid_labels = labels[validation_idxs]
 
             train_examples_queues[dataset_id] = get_readers_for_dataset(
-                train_intervals, datafiles, train_labels, name='{}-training'.format(dataset_id),
+                train_intervals, datafiles, train_labels, name='{}-training'.format(
+                    dataset_id),
                 in_memory=in_memory)
-            if not no_validation_queues:
-                valid_examples_queues[dataset_id] = get_readers_for_dataset(
-                    valid_intervals, datafiles, labels=valid_labels,
-                    name='{}-validation'.format(dataset_id), read_batch_size=1, in_memory=in_memory)
 
-    return train_examples_queues, valid_examples_queues, task_names
+    return train_examples_queues, task_names
+
+
+def get_valid_readers_and_tasknames(processed_inputs_file, processed_intervals_file,
+                                    validation_chroms=[], holdout_chroms=[],
+                                    name='data-readers', no_validation_queues=False,
+                                    in_memory=True):
+    """Generate two reader and examples queues for train/validation of each dataset.
+
+    Args:
+        processed_inputs_file: json file with processed input data directories for each dataset
+        processed_intervals_file: json file with processed intervals files for each dataset
+        validation_chroms: (optional) list of chrom strings to provide as validation examples
+        holdout_chroms: (optional) list of heldout chrom strings that will not be provided
+
+    Returns: a tuple of (validation_readers, task_names):
+
+        validation_readers: a dictionary of an examples queue for each dataset. Each queue contains:
+            intervals/start (int) - the start coordinate of the intervals
+            intervals/end (int) - the end coordinate of the intervals
+            intervals/chrom (string) - the chroms of the intervals
+            data/{data_name} (e.g. `data/dnase_data_dir` or `data/genome_data_dir`) - the actual
+                data fields that were read and possibly normalized
+
+            iff labels are specified in the `processed_intervals_file`, also:
+            labels - the labels for each interval for each task (size T)
+        task names:
+            a list of strings of task names, of size T
+    """
+    with tf.variable_scope(name):
+        # Check validation and holdout chrom sets are mutually exclusive
+        validation_and_holdout_chroms = set(
+            validation_chroms).intersection(holdout_chroms)
+        if len(validation_and_holdout_chroms) != 0:
+            raise IOError('Some chroms were specified for both validation and holdout: {}'.format(
+                validation_and_holdout_chroms))
+
+        datasets = parse_inputs_and_intervals(
+            processed_inputs_file, processed_intervals_file)
+        task_names = datasets[list(datasets.keys())[0]]['task_names']
+
+        valid_examples_queues = {}
+        for dataset_id, dataset in datasets.items():
+            intervals = dataset['intervals']
+
+            validation_idxs = np.in1d(intervals['chrom'], validation_chroms)
+            valid_intervals = {k: intervals[k][validation_idxs]
+                               for k in ['chrom', 'start', 'end']}
+
+            datafiles = dataset['inputs']
+            labels = None
+            valid_labels = None
+            if 'labels' in dataset.keys():
+                labels = dataset['labels']
+                valid_labels = labels[validation_idxs]
+
+            valid_examples_queues[dataset_id] = get_readers_for_dataset(
+                valid_intervals, datafiles, labels=valid_labels,
+                name='{}-validation'.format(dataset_id), read_batch_size=1, in_memory=in_memory)
+
+    return valid_examples_queues, task_names
 
 
 def get_readers_for_dataset(intervals, datafiles, labels=None, name='dataset-reader',
@@ -153,10 +201,13 @@ def get_readers_for_dataset(intervals, datafiles, labels=None, name='dataset-rea
         read_values = {}
         for k, datafile in datafiles.items():
             reader_kwargs = {}
-            reader_kwargs['norm_params'] = 'local_zscore' if k == 'dnase_data_dir' else None
-            reader_kwargs['interval_params'] = 'midpoint' if k == 'dnase_peaks_counts_data_dir' else None
+            reader_kwargs[
+                'norm_params'] = 'local_zscore' if k == 'dnase_data_dir' else None
+            reader_kwargs[
+                'interval_params'] = 'midpoint' if k == 'dnase_peaks_counts_data_dir' else None
             read_values[k] = bcolz_interval_reader(
-                to_read, datafile, interval_size, op_name='{}-reader'.format(k),
+                to_read, datafile, interval_size, op_name='{}-reader'.format(
+                    k),
                 in_memory=in_memory, **reader_kwargs)
 
         # Enqueue the read values, along with labels and intervals
--- tfdragonn/tensorflow/train.py
+++ tfdragonn/tensorflow/train.py
@@ -8,7 +8,8 @@ import shutil
 
 import tensorflow as tf
 
-from dataset_interval_reader import get_readers_and_tasks_with_holdouts
+from dataset_interval_reader import get_train_readers_and_tasknames
+from dataset_interval_reader import get_valid_readers_and_tasknames
 from shared_examples_queue import SharedExamplesQueue
 from shared_examples_queue import ValidationSharedExamplesQueue
 from models import SequenceAndDnaseClassifier
@@ -19,6 +20,8 @@ VALID_CHROMS = ['chr9']
 
 TRAIN_DIRNAME = 'train'
 VALID_DIRNAME = 'valid'
+
+IN_MEMORY = False
 
 logging.basicConfig(
     format='%(levelname)s %(asctime)s %(message)s', level=logging.DEBUG)
@@ -48,26 +51,42 @@ logging.info('intervalspec file: {}'.format(args.intervalspec))
 logging.info('logdir path: {}'.format(args.logdir))
 logging.info('visiblegpus string: {}'.format(args.visiblegpus))
 
+train_graph = tf.Graph()
+valid_graph = tf.Graph()
+
 logging.info('Setting up readers')
 
-train_readers, valid_readers, task_names = get_readers_and_tasks_with_holdouts(
-    args.datasetspec, args.intervalspec, validation_chroms=VALID_CHROMS,
-    holdout_chroms=HOLDOUT_CHROMS, no_validation_queues=False, in_memory=False)
-train_shared_queue = SharedExamplesQueue(train_readers, task_names, batch_size=128)
-valid_shared_queue = ValidationSharedExamplesQueue(valid_readers, task_names, batch_size=128)
+with train_graph.as_default():
+    train_readers, task_names = get_train_readers_and_tasknames(
+        args.datasetspec, args.intervalspec, validation_chroms=VALID_CHROMS,
+        holdout_chroms=HOLDOUT_CHROMS, in_memory=IN_MEMORY)
+    train_shared_queue = SharedExamplesQueue(train_readers, task_names, batch_size=128)
+
+with valid_graph.as_default():
+    valid_readers, task_names = get_valid_readers_and_tasknames(
+        args.datasetspec, args.intervalspec, validation_chroms=VALID_CHROMS,
+        holdout_chroms=HOLDOUT_CHROMS, in_memory=IN_MEMORY)
+    valid_shared_queue = ValidationSharedExamplesQueue(valid_readers, task_names, batch_size=128)
 
 logging.info('Setting up model')
 
 num_tasks = len(task_names)
-model = SequenceAndDnaseClassifier(num_tasks=num_tasks, fc_layer_widths=(800, 300, 50))
+with train_graph.as_default():
+    train_model = SequenceAndDnaseClassifier(num_tasks=num_tasks, fc_layer_widths=(800, 300, 50))
+    trainer = ClassiferTrainer(train_model, num_epochs=1, epoch_size=10000)
+with valid_graph.as_default():
+    valid_model = SequenceAndDnaseClassifier(num_tasks=num_tasks, fc_layer_widths=(800, 300, 50))
+    evaluator = ClassiferTrainer(valid_model)
 
 logging.info('Starting training')
 
 session_config = tf.ConfigProto()
 session_config.gpu_options.deferred_deletion_bytes = int(250 * 1e6)  # 250MB
 session_config.gpu_options.visible_device_list = args.visiblegpus
-trainer = ClassiferTrainer(model)
-checkpoint = trainer.train(train_shared_queue, train_log_dir, session_config=session_config)
+
+with train_graph.as_default():
+    checkpoint = trainer.train(train_shared_queue, train_log_dir, session_config=session_config)
 
 # logging.info('Starting eval')
-# trainer.evaluate(valid_shared_queue, valid_log_dir, checkpoint, session_config)
+with valid_graph.as_default():
+    trainer.evaluate(valid_shared_queue, valid_log_dir, checkpoint, session_config)

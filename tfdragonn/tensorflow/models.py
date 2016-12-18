43be21a989e5c64a511ca8f0b763f3b13eccdaaa
jisraeli <jisraeli@stanford.edu>
2016-12-17 19:54:38 -0800

    added SequenceDnaseAndDnasePeaksCountsClassifier

--- tfdragonn/tensorflow/models.py
+++ tfdragonn/tensorflow/models.py
@@ -107,3 +107,103 @@ class SequenceAndDnaseClassifier(Classifier):
                 logits, self.num_tasks, activation_fn=None, scope='output-fc')
 
             return logits
+
+
+class SequenceDnaseAndDnasePeaksCountsClassifier(Classifier):
+
+    @property
+    def get_inputs(self):
+        return ["data/genome_data_dir",
+                "data/dnase_data_dir",
+                "data/dnase_peaks_counts_data_dir"]
+
+    def __init__(self, num_tasks=1,
+                 num_seq_filters=(25, 25, 25), seq_conv_width=(25, 25, 25),
+                 num_dnase_filters=(25, 25, 25), dnase_conv_width=(25, 25, 25),
+                 num_combined_filters=(55,), combined_conv_width=(25,),
+                 peaks_counts_fc_layer_widths=(20,),
+                 final_fc_layer_widths=(), pool_width=25, batch_norm=False):
+        assert len(num_seq_filters) == len(seq_conv_width)
+        assert len(num_dnase_filters) == len(dnase_conv_width)
+        assert len(num_combined_filters) == len(combined_conv_width)
+
+        self._num_tasks = num_tasks
+        self.num_seq_filters = num_seq_filters
+        self.seq_conv_width = seq_conv_width
+        self.num_dnase_filters = num_dnase_filters
+        self.dnase_conv_width = dnase_conv_width
+        self.num_combined_filters = num_combined_filters
+        self.combined_conv_width = combined_conv_width
+        self.peaks_counts_fc_layer_widths = peaks_counts_fc_layer_widths
+        self.final_fc_layer_widths = final_fc_layer_widths
+        self.pool_width = pool_width
+        self.batch_norm = batch_norm
+
+    def get_logits(self, inputs):
+        with slim.arg_scope(
+                [slim.conv2d, slim.fully_connected], reuse=False, activation_fn=tf.nn.relu,
+                weights_initializer=initializers.he_normal_initializer(),
+                biases_initializer=tf.constant_initializer(0.0)):
+
+            def expand_4D(input_tensor):
+                shape = [x.value for x in input_tensor.get_shape()]
+                if len(shape) == 2:  # 1-D input
+                    new_shape = [shape[0], 1, shape[1], 1]
+                elif len(shape) == 3:  # 2-D input
+                    new_shape = shape + [1]
+                else:
+                    raise IOError('unrecognized shape: {}'.format(shape))
+                return tf.reshape(input_tensor, new_shape)
+
+            seq_preds = inputs["data/genome_data_dir"]
+            seq_preds = expand_4D(seq_preds)
+            for i, (num_filter, filter_width) in enumerate(
+                    zip(self.num_seq_filters, self.seq_conv_width)):
+                filter_height = 4 if i == 0 else 1
+                filter_dims = [filter_height, filter_width]
+                seq_preds = slim.conv2d(seq_preds, num_filter, filter_dims, padding='VALID',
+                                        scope='sequence_conv{:d}'.format(i + 1))
+
+            dnase_preds = inputs["data/dnase_data_dir"]
+            dnase_preds = expand_4D(dnase_preds)
+            for i, (num_filter, filter_width) in enumerate(
+                    zip(self.num_dnase_filters, self.dnase_conv_width)):
+                fitler_dims = [1, filter_width]
+                dnase_preds = slim.conv2d(dnase_preds, num_filter, fitler_dims, padding='VALID',
+                                          scope='dnase_conv{:d}'.format(i + 1))
+
+            # check if concatenation axis is correct
+            print('seq_preds shape {}'.format(seq_preds.get_shape()))
+            print('dnase_preds shape {}'.format(dnase_preds.get_shape()))
+            seq_dnase_preds = tf.concat(1, [seq_preds, dnase_preds])
+            print('concat shape {}'.format(seq_dnase_preds.get_shape()))
+            for i, (num_filter, filter_width) in enumerate(
+                    zip(self.num_combined_filters, self.combined_conv_width)):
+                filter_height = 2 if i == 0 else 1
+                filter_dims = [filter_height, filter_width]
+                seq_dnase_preds = slim.conv2d(seq_dnase_preds, num_filter, filter_dims, padding='VALID',
+                                     scope='combined_conv{:d}'.format(i + 1))
+            print('after combined conv layers shape {}'.format(seq_dnase_preds.get_shape()))
+            seq_dnase_preds = slim.avg_pool2d(seq_dnase_preds, [1, self.pool_width], stride=[1, self.pool_width],
+                                     padding='VALID', scope='avg_pool')
+            seq_dnase_preds = slim.flatten(seq_dnase_preds, scope='flatten')
+            print('after flattening sequence and dnase shape {}'.format(seq_dnase_preds.get_shape()))
+
+            # fully connect dnase peaks counts and concatenate
+            peaks_counts_preds = inputs["data/dnase_peaks_counts_data_dir"]
+            peaks_counts_preds = slim.flatten(peaks_counts_preds, scope='flatten_peaks_counts')
+            print('peaks_counts_preds before fc layers shape {}'.format(peaks_counts_preds.get_shape()))
+            if len(self.peaks_counts_fc_layer_widths) > 0:
+                peaks_counts_preds = slim.stack(peaks_counts_preds, slim.fully_connected,
+                                                self.peaks_counts_fc_layer_widths, scope='fc_peaks_counts')
+            print('peaks_counts_preds after fc layers shape {}'.format(peaks_counts_preds.get_shape()))
+            logits = tf.concat(1, [seq_dnase_preds, peaks_counts_preds])
+            print('concat peaks_counts_preds and seq_dnase_preds shape {}'.format(logits.get_shape()))
+
+            # fully connect everything
+            if len(self.final_fc_layer_widths) > 0:
+                logits = slim.stack(logits, slim.fully_connected, self.final_fc_layer_widths, scope='fc')
+            logits = slim.fully_connected(
+                logits, self.num_tasks, activation_fn=None, scope='output-fc')
+
+            return logits

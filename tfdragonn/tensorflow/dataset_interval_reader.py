d28de29ec7907b38785a5f95f684a6397d298e46
jisraeli <jisraeli@stanford.edu>
2016-12-17 18:27:23 -0800

    added missing import

--- tfdragonn/tensorflow/dataset_interval_reader.py
+++ tfdragonn/tensorflow/dataset_interval_reader.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from collections import defaultdict
 import tensorflow as tf
 import numpy as np
 

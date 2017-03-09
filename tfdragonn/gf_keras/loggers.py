1c8121134121215ba12eb37e8a74445ba40acb92
jisraeli <jisraeli@stanford.edu>
2017-03-08 21:07:51 -0800

    added loggers

---
+++ tfdragonn/gf_keras/loggers.py
@@ -0,0 +1,7 @@
+import logging
+
+def setup_logger(logger_name, log_file, level=logging.INFO):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(level)
+    file_handler = logging.FileHandler(log_file, mode='w')
+    logger.addHandler(file_handler)
